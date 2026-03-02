dd4bf4d201a5ea42d90b5ad1a9f4c44a666890f1
PhilKMcC <pkmccaffrey@loyola.edu>
2026-03-01 19:24:39 -0600

    started figuring out input

---
+++ Assets/Scripts/testPlayerScript.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class testPlayerScript : MonoBehaviour
+{
+    public InputActionAsset actions;
+
+    public InputAction MoveAction;
+
+    public Rigidbody2D myBody;
+
+    public float MoveSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MoveAction = InputSystem.actions.FindAction("Player/Move");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        myBody.linearVelocity = (move * MoveSpeed);
+    }
+
+    private void OnEnable()
+    {
+        actions.FindActionMap("Player").Enable();
+    }
+
+    private void Awake()
+    {
+    }
+    private void OnDisable()
+    {
+        actions.FindActionMap("Player").Disable();
+    }
+    
+}
