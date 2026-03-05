271383f6ad75bc38f9694b76f0c4206332c1f05f
PhilKMcC <pkmccaffrey@loyola.edu>
2026-03-04 22:06:17 -0600

    added elevator that can push you out

---
+++ Assets/Scripts/elevatorCaller.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using UnityEngine;
+
+public class elevatorCaller : MonoBehaviour, I_Interactable
+{
+    /*
+     * Class Explanation:
+     * Calls the assigned eleavtor to the location A. after a while returns it to location B
+     * Added this because I want to see what happens if player is crushed between floor and elevator.
+     */
+
+
+    public GameObject Elevator;
+    public Vector3 locationA;
+    public Vector3 locationB;
+    private Vector3 offset; //locationB -locationA
+
+    public float callDuration = 5;
+    public float timer = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timer = callDuration * 3;
+        offset = locationA - locationB;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < callDuration * 3)
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    public void Interact()
+    {
+        if (timer >= callDuration * 3)
+        {
+            Debug.Log("ElevatorCalled");
+            StartCoroutine(CallElevator());
+        }
+    }
+
+    IEnumerator CallElevator()
+    {
+        timer = 0;
+        Vector3 start = Elevator.transform.position;
+        while(timer < callDuration) {
+            Vector3 pos = start + offset * (timer/callDuration);
+            Elevator.transform.position = pos;
+            yield return null;
+        }
+        yield return new WaitForSeconds(callDuration);
+        while(timer < callDuration * 3)
+        {
+            Vector3 pos = locationA - offset * ((timer - callDuration * 2) / callDuration);
+            Elevator.transform.position = pos;
+            yield return null;
+        }
+
+
+        yield return null;
+    }
+
+}
