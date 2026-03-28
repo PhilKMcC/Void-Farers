623c655f05d1f65a402e42a79cde508d360c378f
UnsaidMushroom <ultimategamerst@gmail.com>
2026-03-28 21:01:34 +0000

    Undid faulty code

--- Assets/Scripts/Other/Beam.cs
+++ Assets/Scripts/Other/Beam.cs
@@ -25,7 +25,7 @@ public class Beam : abstrGolem
     // Update is called once per frame
     void Update()
     {
-        if(state == )
+        if(state == 5 && !wait) {
         timer -= Time.deltaTime;
         Rotate();
         if (!damaging && timer <= startDamaging)
