3bd8dcae0d0de46bebcd5f7478ff3df9658ebf36
UnsaidMushroom <ultimategamerst@gmail.com>
2026-03-29 20:34:55 +0100

    Crystal Iframes and Animation

--- Assets/Scripts/Damagables/golemCrystal.cs
+++ Assets/Scripts/Damagables/golemCrystal.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework.Internal;
+using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class golemCrystal : Abstr_Damagable
@@ -11,25 +14,50 @@ public class golemCrystal : Abstr_Damagable
 
     public float healthTotal = 3f;
     private float health;
+    private bool damagable;
+    private float timer = 3;
+    private Vector3 startPos;
+    private Vector3 rightPos;
+    private Vector3 leftPos;
+    private int animState = 0;
+    private float distanceVector;
 
     void Start()
     {
         health = 3f;
         myTag = gameObject.tag;
+        startPos = transform.position;
+        rightPos = startPos;
+        leftPos = startPos;
+        rightPos.x += 2;
+        leftPos.x -= 2;
         //initializeSets();
     }
 
     void Update()
     {
-        //Deal with states here?
+        if(!damagable)
+        {
+            timer -= Time.deltaTime;
+            if(timer <= 0)
+            {
+                damagable = true;
+                timer = 3;
+                animState = 0;
+            }
+        }
     }
     public override void Damage()
     {
-        health--;
-        //Hurt animation
-        if(health == 0)
+        if (damagable && golemController.state != 0)
         {
-            Die();
+            health--;
+            hurtAnim();
+            if (health == 0)
+            {
+                Die();
+            }
+            damagable = false;
         }
     }
 
@@ -49,5 +77,71 @@ public class golemCrystal : Abstr_Damagable
     {
         health = healthTotal;
     }
-
+    
+    void hurtAnim()
+    {
+        while (animState != 5)
+        {
+            switch (animState)
+            {
+                case 0:
+                    distanceVector = Vector2.Distance(transform.position, rightPos);
+                    if (distanceVector != 0)
+                    {
+                        transform.position = Vector2.MoveTowards(transform.position, rightPos, 10 * Time.deltaTime);
+                    }
+                    else
+                    {
+                        animState = 1;
+                    }
+                    break;
+                case 1:
+                    distanceVector = Vector2.Distance(transform.position, leftPos);
+                    if (distanceVector != 0)
+                    {
+                        transform.position = Vector2.MoveTowards(transform.position, leftPos, 10 * Time.deltaTime);
+                    }
+                    else
+                    {
+                        animState = 2;
+                    }
+                    break;
+                case 2:
+                    distanceVector = Vector2.Distance(transform.position, rightPos);
+                    if (distanceVector != 0)
+                    {
+                        transform.position = Vector2.MoveTowards(transform.position, rightPos, 10 * Time.deltaTime);
+                    }
+                    else
+                    {
+                        animState = 3;
+                    }
+                    break;
+                case 3:
+                    distanceVector = Vector2.Distance(transform.position, leftPos);
+                    if (distanceVector != 0)
+                    {
+                        transform.position = Vector2.MoveTowards(transform.position, leftPos, 10 * Time.deltaTime);
+                    }
+                    else
+                    {
+                        animState = 4;
+                    }
+                    break;
+                case 4:
+                    distanceVector = Vector2.Distance(transform.position, startPos);
+                    if (distanceVector != 0)
+                    {
+                        transform.position = Vector2.MoveTowards(transform.position, startPos, 10 * Time.deltaTime);
+                    }
+                    else
+                    {
+                        animState = 5;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
